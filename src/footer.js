3f196ddf45c5fb778409d82b990ddb3f422ef28f
YeyoSM <152399977+YeyoSM@users.noreply.github.com>
2024-04-24 17:56:38 +0000

    eu

--- src/footer.js
+++ src/footer.js
@@ -1,5 +1,5 @@
 const Footer = () => {
-    return <footer>
+    return <footer> 
        <div class="col-lg-9 ">
                           <p class="text-center">Lionel Andrés Messi Cuccittini (Rosario, 24 de junio de 1987), conocido como Leo Messi, es un futbolista argentino que juega como delantero o centrocampista. Desde 2023, integra el plantel del Inter Miami de la MLS canadoestadounidense. Es también internacional con la selección de Argentina, de la que es capitán.</p>
                       </div>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 const Main = () => {
     return <main>
-       <div id="carouselExampleIndicators" class="carousel slide">
+<div id="carouselExampleIndicators" class="carousel slide">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner">
+ <div class="carousel-inner">
     <div class="carousel-item active">
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmQDLE5RikXl3Qo-dMRNOZdXB5Dar4v8N0eXa7xHUVni4rirFQUeUZ1_GwHwaQVAh8dLI&usqp=CAU" class="d-block w-100" alt="Messi"></img>
     </div>
-    <div class="carousel-item">
+ <div class="carousel-item">
       <img src="https://i.pinimg.com/736x/0a/af/14/0aaf1423f722b2e79b08cec36a09c61a.jpg" class="d-block w-60" alt="Messi"></img>
     </div>
   </div>
